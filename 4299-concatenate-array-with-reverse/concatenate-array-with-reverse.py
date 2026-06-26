84dea8e9661ffeef4344dc996dbdf9eb0ec76510
Ayush Mishra <itsanshmishra34@gmail.com>
2026-06-26 20:22:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (8.18%) - LeetSync

--- 4299-concatenate-array-with-reverse/concatenate-array-with-reverse.py
+++ 4299-concatenate-array-with-reverse/concatenate-array-with-reverse.py
@@ -1,9 +1,7 @@
 class Solution(object):
-    a = []
     def concatWithReverse(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-        self.a = nums + nums[::-1]
-        return self.a
+        return nums + nums[::-1]
