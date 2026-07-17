4327007ce1a89548892e63816a8aba56b4d829e1
Ayush Mishra <itsanshmishra34@gmail.com>
2026-07-17 16:21:36 +0530

    Time: 3 ms (49.07%) | Memory: 12.5 MB (16.98%) - LeetSync

---
+++ 4321-digit-frequency-score/digit-frequency-score.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def digitFrequencyScore(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        sum = 0
+        for i in str(n):
+            sum+=int(i)
+        return sum
